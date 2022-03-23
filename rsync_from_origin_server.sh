dcad1fa12faaf2c2418cac20839ae160574d7979
informdev <30958815+informdev@users.noreply.github.com>
2022-03-23 13:51:41 -0400

    Create rsync_from_origin_server.sh

---
+++ rsync_from_origin_server.sh
@@ -0,0 +1,5 @@
+#repeat the variables as much or as little as needed.
+
+rsync -a /PATH/TO/SITE/FILES/ root@111.111.111.111:/home/CPANEL_USERNAME/public_html;
+rsync -a /PATH/TO/SITE/FILES/ root@111.111.111.111:/home/CPANEL_USERNAME/public_html;
+rsync -a /PATH/TO/SITE/FILES/ root@111.111.111.111:/home/CPANEL_USERNAME/public_html;

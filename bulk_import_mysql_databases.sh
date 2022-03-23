78dc9b063e3d056b7185f874c2b3403bf2122c7c
informdev <30958815+informdev@users.noreply.github.com>
2022-03-23 13:50:54 -0400

    Create bulk_import_mysql_databases.sh

---
+++ bulk_import_mysql_databases.sh
@@ -0,0 +1,6 @@
+#for my use at least you can't have a space between -p and the DB password.
+#repeat these variables as much or as little as needed.
+
+mysql -u DB_USERNAME -pDB_PASSWORD DB_NAME < DB_EXPORT.sql;
+mysql -u DB_USERNAME -pDB_PASSWORD DB_NAME < DB_EXPORT.sql;
+mysql -u DB_USERNAME -pDB_PASSWORD DB_NAME < DB_EXPORT.sql;
